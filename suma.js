e51eb2d37d4b69f840c55c09c56e31edb1387d05
admin <admin@virtual.centos7>
2018-09-27 17:52:22 -0500

    aprimer programa

---
+++ suma.js
@@ -0,0 +1,10 @@
+//Este comando te sacara el resultado de la siguiente suma function
+function suma (){
+var numero1 = 2;
+var numero2 = 3;
+console.log(numero1 + "+" +numero2+ "="+(numero1 + numero2));
+var resultado=numero1+numero2;
+return resultado;
+}
+var imprime = suma();
+console.log(imprime);
